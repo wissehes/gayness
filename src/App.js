a19c9f198e546bbec469b16ce0d7590ffc110e10
Wisse <wissehes@gmail.com>
2022-09-21 11:52:31 +0200

    yassify it. 💁‍♀️

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [slay, setSlay] = useState(0);
+  const [yassified, setYassified] = useState(false);
+  const [yassification, setYassication] = useState(0);
+
+  const countup = () => setSlay(slay + 1);
+  const yassify = () => {
+    const slAy = getRandomSlay();
+
+    setSlay(slay + slAy);
+    setYassication(slAy);
+    setYassified(true);
+  };
+
+  const getRandomSlay = () => Math.round(Math.random() * 10);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="maintext">
+          Did <span class="thick">you</span>{" "}
+          <p class="slay" onClick={yassify}>
+            slay
+          </p>
+          today?
+        </h1>
+        <div class="slayclass">
+          {/* <button onClick={countup}>slay.</button> */}
+
+          <button class="slaybutton" onClick={countup}>
+            SLAY
+          </button>
+
+          <p>
+            You slayed <span class="slaycount">{slay}</span> times.
+          </p>
+          {yassified && (
+            <p class="yassified">YOU YASSIFIED! +{yassification} slays.</p>
+          )}
+        </div>
       </header>
     </div>
   );
